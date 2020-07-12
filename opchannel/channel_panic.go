c8b647b5aa36a3a812a7ecf363ca515b3bc82787
GeekWho <geekwho_eth@outlook.com>
2020-07-12 19:26:05 +0800

    feat: 新增channel map 相关示例

--- opchannel/channel_panic.go
+++ opchannel/channel_panic.go
@@ -26,3 +26,29 @@ func sendData2CloseChannel() {
 	// 发送数据到已关闭的channel，会产生panic
 	ch1 <- 4
 }
+
+func createNoBufferedChannelPanic() {
+	ch := make(chan int) // 创建一个无缓冲的通道
+	ch <- 1              // 主 goroutine 在这里阻塞等待接收者，但没有接收者 —— panic
+}
+
+func createBufferedChannelPanic() {
+	ch := make(chan int, 1)
+	ch <- 1
+}
+
+func fixedChannelPanic() {
+	ch := make(chan int)
+
+	go func() {
+		fmt.Println("Received:", <-ch)
+	}()
+
+	ch <- 1 // 正常发送，不再 panic
+}
+
+func fixedBufferedChannel() {
+	ch := make(chan int, 1)
+	ch <- 1 // 不会阻塞，因为 channel 有一个缓冲位
+	fmt.Println("Sent to buffered channel")
+}
